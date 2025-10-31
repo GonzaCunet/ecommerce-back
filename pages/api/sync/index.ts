10be17cea650e68e774087fb482db05f87d3f995
Gonza <gonza.cunet@outlook.com>
2025-10-31 10:29:12 -0300

    Refactor sync API to improve error handling and response messages

--- pages/api/sync/index.ts
+++ pages/api/sync/index.ts
@@ -3,35 +3,26 @@ import { Airtablebase } from "lib/airtable";
 import { client, indexProduct } from "lib/algolia";
 
 export default function (req: NextApiRequest, res: NextApiResponse) {
-  // const { offset, limit } = getOffsetAndLimitFromReq(req, 100, 100000);
+  //   const { offset, limit } = getOffsetAndLimitFromReq(req, 100, 10000);
   Airtablebase("Furniture")
-    .select({
-      pageSize: 10,
-    })
-    .eachPage(async function (records, fetchNextPage) {
-      const objects = records.map((r) => {
-        return {
-          objectID: r.id,
-          ...r.fields,
-        };
-      });
-
-      try {
+    .select({ pageSize: 10 })
+    .eachPage(
+      async function (records, fetchNextPage) {
+        const objects = records.map((r) => {
+          return { objectID: r.id, ...r.fields };
+        });
         const index = client.initIndex(indexProduct);
         await index.saveObjects(objects);
-        console.log("siguiente página");
+
         fetchNextPage();
-      } catch (error) {
-        res.status(500).json({ message: "error" });
-        console.log(error);
-      }
+      },
       function done(err) {
         if (err) {
           console.log(err);
           return;
         }
-        console.log("done");
-        res.status(200).json({ message: "done" });
+        console.log("termino");
+        res.status(200).send({ message: "ok" });
       }
-    });
+    );
 }
